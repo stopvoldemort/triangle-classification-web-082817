16a3bcb6a5e71fd9eb0253bc6aa43208de967f30
stopvoldemort <david.squires1@gmail.com>
2017-09-03 14:53:53 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+    raise TriangleError if self.no_length || too_long
+  end
+
+  def kind
+    if @length_one == @length_two && @length_two == @length_three
+      :equilateral
+    elsif @length_one == @length_two || @length_one == @length_three || @length_two == @length_three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def no_length
+    @length_one <= 0 || @length_two <= 0 || @length_three <= 0
+  end
+
+  def too_long
+    sorted_lengths = [@length_one, @length_two, @length_three].sort
+    sorted_lengths[2] >= sorted_lengths[1] + sorted_lengths[0]
+  end
+
+
+
+end
+
+class TriangleError < StandardError
+  puts "Those lengths don't make sense for a triangle."
 end
